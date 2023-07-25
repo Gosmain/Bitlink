63b61cc392ae8515a743000c0354bc3097f97dbb
Replit user <>
2023-07-25 13:39:11 +0000

    short link funk

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import requests
 
-headers = {
+def short_link(url):
+  
+  headers = {
     'Authorization': 'Bearer 7513bd6051bacfb260ef66a4a4ab35e27a74b683',
     'Content-Type': 'application/json',
 }
+  data = f'{{ "long_url": "{url}", "domain": "bit.ly"}}'
 
-data = '{ "long_url": "https://kanikuli-v-meksike-tv.ru/2-sezon/56-seriya.html", "domain": "bit.ly"}'
+  response = requests.post('https://api-ssl.bitly.com/v4/shorten', headers=headers, data=data)
 
-response = requests.post('https://api-ssl.bitly.com/v4/shorten', headers=headers, data=data)
+  return response.json()['id']
 
-print(response.json()['id'])
+print('Enter an address to create a short link')
+url_for_short = input()
+print(f'Short link to use:\n{short_link(url_for_short)}')
