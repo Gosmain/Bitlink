d19ca3170f2f3e6f9595efc9a3bc409aef54533c
Replit user <>
2023-08-02 07:21:27 +0000

    TODO

--- parser/my_parse_util.py
+++ parser/my_parse_util.py
@@ -10,23 +10,15 @@ def get_dir_name(path):
 def create_dir(dir_name):
     os.makedirs(dir_name)
 
+def comp(cmp):
+  if cmp == None:
+    return ''
+  else:
+    return cmp
 
-def enter_info(date_tpl):
-    # TODO date_tpl прям плохо, почему дата(date)? Не забывай, что тип данных в названии не используем
-    name, description, short_link = date_tpl
-    if name and description:
-        if name:
-            return f'{name} {description} - {short_link}'
-        else:
-            return f'{short_link}'
-    else:
-        if name:
-            return f'{name} - {short_link}'
-        else:
-            return f'{description} - {short_link}'
-
-        # TODO подймай, как можно избавиться от огромного дерева ифов
-
+def enter_info(tpl):
+    name, description, short_link = list(map(comp, tpl))
+    return f'{name} {description} - {short_link}'.strip(' -')
 
 def parse_console():
     parser = argparse.ArgumentParser()
